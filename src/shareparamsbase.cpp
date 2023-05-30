0888b8be580d54d8126a290389c7a20f88e15504
Mark Friedenbach <mark@friedenbach.org>
2023-05-30 12:00:40 -0700

    [ShareChain] Add ShareChainParams, which describes the configured share chain.

---
+++ src/shareparamsbase.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2020-2023 The Freicoin Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <sharechain.h> // for ShareChainParams
+
+#include <string> // for std::string
+
+const std::string ShareChainParams::SOLO = "solo";
+const std::string ShareChainParams::MAIN = "main";
+
+// End of File
