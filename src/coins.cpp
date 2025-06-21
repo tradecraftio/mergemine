tradecraftio/tradecraft

[FinalTx] Use std::optional to track whether cache is filled or not.
Explicity record, via std::optional container, whether hashBestBlock has been stored in the cache, allowing it to be fetched before flushing if not set, and therefore only changing the on-disk value if it has been updated.

src/coins.cpp
@@ -167,9 +167,10 @@ bool CCoinsViewCache::HaveCoinInCache(const COutPoint &outpoint) const {
 }
 
 uint256 CCoinsViewCache::GetBestBlock() const {
-    if (hashBlock.IsNull())
+    if (!hashBlock) {
         hashBlock = base->GetBestBlock();
-    return hashBlock;
+    }
+    return *hashBlock;
 }
 
 void CCoinsViewCache::SetBestBlock(const uint256 &hashBlockIn) {
@@ -233,7 +234,7 @@ bool CCoinsViewCache::BatchWrite(CCoinsMap &mapCoins, const uint256 &hashBlockIn
 }
 
 bool CCoinsViewCache::Flush() {
-    bool fOk = base->BatchWrite(cacheCoins, hashBlock);
+    bool fOk = base->BatchWrite(cacheCoins, GetBestBlock());
     cacheCoins.clear();
     cachedCoinsUsage = 0;
     return fOk;

tradecraftio/mergemine

src/coins.cpp
@@ -171,9 +171,10 @@ bool CCoinsViewCache::HaveCoinInCache(const COutPoint &outpoint) const {
 }
 
 uint256 CCoinsViewCache::GetBestBlock() const {
-    if (hashBlock.IsNull())
+    if (!hashBlock) {
         hashBlock = base->GetBestBlock();
-    return hashBlock;
+    }
+    return *hashBlock;
 }
 
 void CCoinsViewCache::SetBestBlock(const uint256 &hashBlockIn) {
@@ -249,7 +250,7 @@ bool CCoinsViewCache::BatchWrite(CoinsViewCacheCursor& cursor, const uint256 &ha
 
 bool CCoinsViewCache::Flush() {
     auto cursor{CoinsViewCacheCursor(cachedCoinsUsage, m_sentinel, cacheCoins, /*will_erase=*/true)};
-    bool fOk = base->BatchWrite(cursor, hashBlock);
+    bool fOk = base->BatchWrite(cursor, GetBestBlock());
     if (fOk) {
         cacheCoins.clear();
         ReallocateCache();
@@ -261,7 +262,7 @@ bool CCoinsViewCache::Flush() {
 bool CCoinsViewCache::Sync()
 {
     auto cursor{CoinsViewCacheCursor(cachedCoinsUsage, m_sentinel, cacheCoins, /*will_erase=*/false)};
-    bool fOk = base->BatchWrite(cursor, hashBlock);
+    bool fOk = base->BatchWrite(cursor, GetBestBlock());
     if (fOk) {
         if (m_sentinel.second.Next() != &m_sentinel) {
             /* BatchWrite must clear flags of all entries */