tradecraftio/tradecraft

[FinalTx] Add BIP9 deployment logic for block-final transactions.
The dates used are dummy placeholders, to be filled in with actual deployment dates in a later commit.

src/consensus/params.h
@@ -31,6 +31,7 @@ enum DeploymentPos
 {
     DEPLOYMENT_TESTDUMMY,
     DEPLOYMENT_LOCKTIME, // Deployment of BIP68 and BIP113.
+    DEPLOYMENT_BLOCKFINAL, // Deployment of block-final miner commitment transaction.
     // NOTE: Also add new deployments to VersionBitsDeploymentInfo in versionbits.cpp
     MAX_VERSION_BITS_DEPLOYMENTS
 };

tradecraftio/mergemine

src/consensus/params.h
@@ -32,6 +32,7 @@ constexpr bool ValidDeployment(BuriedDeployment dep) { return dep <= DEPLOYMENT_
 enum DeploymentPos : uint16_t {
     DEPLOYMENT_TESTDUMMY,
     DEPLOYMENT_TAPROOT, // Deployment of Schnorr/Taproot (BIPs 340-342)
+    DEPLOYMENT_FINALTX, // Deployment of block-final miner commitment transaction.
     // NOTE: Also add new deployments to VersionBitsDeploymentInfo in deploymentinfo.cpp
     MAX_VERSION_BITS_DEPLOYMENTS
 };